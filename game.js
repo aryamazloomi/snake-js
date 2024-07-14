bb41048182ab8504e665fc420bea2ebf8695e08b
aryamazloomi <47749135+aryamazloomi@users.noreply.github.com>
2024-07-13 23:03:59 -0400

    Update game.js

--- game.js
+++ game.js
@@ -19,68 +19,58 @@ let score = 0;
 let scoreText;
 
 function preload() {
-    this.load.image('snake', 'https://yourwebsite.com/path/to/snake.png');
-    this.load.image('food', 'https://yourwebsite.com/path/to/apple.gif');
+    // Load assets
+    this.load.image('food', 'apple.gif');  // Ensure this path is correct
 }
 
 function create() {
-    snake = this.physics.add.group();
+    // Create the snake head
+    snake = this.add.group();
+    const head = this.add.rectangle(300, 300, 20, 20, 0x00ff00);
+    this.physics.add.existing(head);
+    snake.add(head);
 
-    // Initial snake body
-    for (let i = 0; i < 3; i++) {
-        snake.create(300 + i * 20, 300, 'snake');
-    }
-
+    // Create food
     food = this.physics.add.image(Phaser.Math.Between(0, 600), Phaser.Math.Between(0, 600), 'food');
+    this.physics.add.collider(head, food, eatFood, null, this);
+
+    // Create cursors for keyboard input
     cursors = this.input.keyboard.createCursorKeys();
 
+    // Create score text
     scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#000' });
+
+    this.physics.add.overlap(head, food, eatFood, null, this);
 }
 
 function update() {
     let head = snake.getChildren()[0];
 
-    if (cursors.left.isDown && head.direction !== 'right') {
-        head.direction = 'left';
-    } else if (cursors.right.isDown && head.direction !== 'left') {
-        head.direction = 'right';
-    } else if (cursors.up.isDown && head.direction !== 'down') {
-        head.direction = 'up';
-    } else if (cursors.down.isDown && head.direction !== 'up') {
-        head.direction = 'down';
-    }
-
-    if (head.direction) {
-        moveSnake(head.direction);
-    }
-
-    if (Phaser.Geom.Intersects.RectangleToRectangle(head.getBounds(), food.getBounds())) {
-        food.setPosition(Phaser.Math.Between(0, 600), Phaser.Math.Between(0, 600));
-        score += 10;
-        scoreText.setText('Score: ' + score);
-        addSegment();
+    if (cursors.left.isDown) {
+        head.setVelocityX(-160);
+        head.setVelocityY(0);
+    } else if (cursors.right.isDown) {
+        head.setVelocityX(160);
+        head.setVelocityY(0);
+    } else if (cursors.up.isDown) {
+        head.setVelocityY(-160);
+        head.setVelocityX(0);
+    } else if (cursors.down.isDown) {
+        head.setVelocityY(160);
+        head.setVelocityX(0);
     }
 }
 
-function moveSnake(direction) {
-    let head = snake.getChildren()[0];
-    let x = head.x;
-    let y = head.y;
-
-    if (direction === 'left') {
-        x -= 20;
-    } else if (direction === 'right') {
-        x += 20;
-    } else if (direction === 'up') {
-        y -= 20;
-    } else if (direction === 'down') {
-        y += 20;
-    }
-
-    head.setPosition(x, y);
+function eatFood(head, food) {
+    food.setPosition(Phaser.Math.Between(0, 600), Phaser.Math.Between(0, 600));
+    score += 10;
+    scoreText.setText('Score: ' + score);
+    addSegment();
 }
 
 function addSegment() {
-    let newSegment = snake.create(0, 0, 'snake');
-    newSegment.setPosition(snake.getChildren()[snake.getLength() - 1].x, snake.getChildren()[snake.getLength() - 1].y);
+    const lastSegment = snake.getChildren()[snake.getLength() - 1];
+    const newSegment = this.add.rectangle(lastSegment.x, lastSegment.y, 20, 20, 0x00ff00);
+    this.physics.add.existing(newSegment);
+    snake.add(newSegment);
 }
